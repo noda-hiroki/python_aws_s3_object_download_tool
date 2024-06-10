8e3ace9c7b50425f7740234827dae41b2d96de1c
noda-hiroki <dhionikora@gmail.com>
2024-06-10 10:44:57 +0900

    Add design document. Implementing basic functions.

--- s3_object_download_tool.py
+++ s3_object_download_tool.py
@@ -3,13 +3,13 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
-def download_file(bucket, object_name, path):
-    os.chdir(path)
-    file_name = object_name
-    s3_client = boto3.client("s3")
-    try:
-        response = s3_client.download_file(bucket, object_name, file_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
+bucket = input("bucket name:")
+file = input("file name:")
+path = input("Download path:")
+s3_client = boto3.client("s3")
+os.chdir(path)
+try:
+    s3_client.download_file(bucket, file, file)
+except ClientError as e:
+    logging.error(e)
+os.system("PAUSE")
