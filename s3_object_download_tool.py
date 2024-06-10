2a195c91a80770a582ac740f654b0fc4a2ff46cd
noda-hiroki <dhionikora@gmail.com>
2024-06-11 06:56:21 +0900

    Added function func_download_files. Left the implementation of the function call pending.

--- s3_object_download_tool.py
+++ s3_object_download_tool.py
@@ -3,6 +3,29 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
+def func_download_files(download_bucket_name: str, target_prefix: str) -> None:
+    """aws s3の指定したバケット内の、指定したプレフィックスを持つファイルを全てダウンロードする。"""
+    s3_client = boto3.client("s3")
+    try:
+    #   指定したバケット内の、指定したプレフィックスを持つファイルのリストを取得する。
+        response = s3_client.list_objects(Bucket=download_bucket_name, Prefix=target_prefix)
+        download_list = [object_data["Key"] for object_data in response["Contents"]]
+        
+    #   ファイルのダウンロード処理
+        for download_key in download_list:
+    #   ディレクトリの場合、何も実行せず次の繰り返し処理に移る。
+            if download_key[-1] == "/":
+                continue
+            download_file_name = os.path.basename(download_key)
+    #   指定したプレフィックスを持つファイルをダウンロード
+            s3_client.download_file(Bucket=download_bucket_name, Key=download_key, Filename=download_file_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+
 #   バケット名、ファイル名、ダウンロード先のパスを入力
 bucket = input("bucket name:")
 file = input("file name:")
