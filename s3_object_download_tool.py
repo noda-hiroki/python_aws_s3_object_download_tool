98bf2d1395ece22b7e0ff3b798c85e6bf81057b1
noda-hiroki <dhionikora@gmail.com>
2024-06-11 01:43:47 +0900

    Added exception handling to AWS S3 operations.

--- s3_object_download_tool.py
+++ s3_object_download_tool.py
@@ -6,19 +6,23 @@ import os
 def func_download_files(download_bucket_name: str, target_prefix: str) -> None:
     """aws s3の指定したバケット内の、指定したプレフィックスを持つファイルを全てダウンロードする。"""
     s3_client = boto3.client("s3")
-#   指定したバケット内の、指定したプレフィックスを持つファイルのリストを取得する。
-    response = s3_client.list_objects(Bucket=download_bucket_name, Prefix=target_prefix)
-    download_list = [object_data["Key"] for object_data in response["Contents"]]
-    
-#   ファイルのダウンロード処理
-    for download_key in download_list:
-#   ディレクトリの場合、何も実行せず次の繰り返し処理に移る。
-        if download_key[-1] == "/":
-            continue
-        download_file_name = os.path.basename(download_key)
-#   指定したプレフィックスを持つファイルをダウンロード
-        s3_client.download_file(Bucket=download_bucket_name, Key=download_key, Filename=download_file_name)
-
+    try:
+    #   指定したバケット内の、指定したプレフィックスを持つファイルのリストを取得する。
+        response = s3_client.list_objects(Bucket=download_bucket_name, Prefix=target_prefix)
+        download_list = [object_data["Key"] for object_data in response["Contents"]]
+        
+    #   ファイルのダウンロード処理
+        for download_key in download_list:
+    #   ディレクトリの場合、何も実行せず次の繰り返し処理に移る。
+            if download_key[-1] == "/":
+                continue
+            download_file_name = os.path.basename(download_key)
+    #   指定したプレフィックスを持つファイルをダウンロード
+            s3_client.download_file(Bucket=download_bucket_name, Key=download_key, Filename=download_file_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 
 #   バケット名、プレフィックス、ダウンロード先のパスを入力
