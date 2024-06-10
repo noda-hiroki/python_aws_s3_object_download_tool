129a05ae0601e12bcb67da75b41804680e9af8eb
noda-hiroki <dhionikora@gmail.com>
2024-06-10 11:16:21 +0900

    The pausing process at the end of processing has been changed from the Windows PAUSE command to the Python input function.

--- s3_object_download_tool.py
+++ s3_object_download_tool.py
@@ -3,13 +3,19 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
+#   バケット名、ファイル名、ダウンロード先のパスを入力
 bucket = input("bucket name:")
 file = input("file name:")
 path = input("Download path:")
+#   s3クライアントインスタンスを作成
 s3_client = boto3.client("s3")
+#   カレントディレクトリを、ダウンロード先ディレクトリに移動
 os.chdir(path)
+
 try:
+#   指定したバケットのファイルをダウンロード
     s3_client.download_file(bucket, file, file)
 except ClientError as e:
     logging.error(e)
-os.system("PAUSE")
+# os.system("PAUSE")
+input("続行するには何かキーを押してください...")
