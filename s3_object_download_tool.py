76b01e17c52cedc0b48c88a38fe028b93924c36b
noda-hiroki <dhionikora@gmail.com>
2024-06-10 16:00:06 +0900

    Implemented recursive downloads

--- s3_object_download_tool.py
+++ s3_object_download_tool.py
@@ -3,19 +3,34 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
-#   バケット名、ファイル名、ダウンロード先のパスを入力
-bucket = input("bucket name:")
-file = input("file name:")
+def func_download_files(download_bucket_name: str, target_prefix: str) -> None:
+    """aws s3の指定したバケット内の、指定したプレフィックスを持つファイルを全てダウンロードする。"""
+    s3_client = boto3.client("s3")
+#   指定したバケット内の、指定したプレフィックスを持つファイルのリストを取得する。
+    response = s3_client.list_objects(Bucket=download_bucket_name, Prefix=target_prefix)
+    download_list = [object_data["Key"] for object_data in response["Contents"]]
+    
+#   ファイルのダウンロード処理
+    for download_key in download_list:
+#   ディレクトリの場合、何も実行せず次の繰り返し処理に移る。
+        if download_key[-1] == "/":
+            continue
+        download_file_name = os.path.basename(download_key)
+#   指定したプレフィックスを持つファイルをダウンロード
+        s3_client.download_file(Bucket=download_bucket_name, Key=download_key, Filename=download_file_name)
+
+
+
+#   バケット名、プレフィックス、ダウンロード先のパスを入力
+bucket_name = input("bucket name:")
+prefix = input("folder(prefix):")
 path = input("Download path:")
-#   s3クライアントインスタンスを作成
-s3_client = boto3.client("s3")
+
 #   カレントディレクトリを、ダウンロード先ディレクトリに移動
 os.chdir(path)
 
-try:
-#   指定したバケットのファイルをダウンロード
-    s3_client.download_file(bucket, file, file)
-except ClientError as e:
-    logging.error(e)
-# os.system("PAUSE")
+#   s3バケットから、指定したプレフィックスのファイルをダウンロード
+func_download_files(bucket_name, prefix)
+
+#   何かキーを入力するまで実行画面を表示
 input("続行するには何かキーを押してください...")
