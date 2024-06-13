4b57a584bef2fa0881b79ae9902768b59f9f65ba
noda-hiroki <dhionikora@gmail.com>
2024-06-13 13:44:09 +0900

    y/nの選択・分岐処理を変更

--- s3_object_download_tool.py
+++ s3_object_download_tool.py
@@ -7,32 +7,36 @@ def func_download_files(download_bucket_name: str, target_prefix: str) -> None:
     """aws s3の指定したバケット内の、指定したプレフィックスを持つファイルを全てダウンロードする。"""
     try:
         while True:
-            select = input("Select whether to set an access key, secret access key, and region.(y/n):")
-            if select == "y":
-                #   アクセスキー、シークレットアクセスキー、リージョンを設定
-                access_key = input("access key:")
-                secret_access_key = input("secret_access_key:")
-                bucket_region = input("s3_bucket_region(Example[Tokyo]: ap-northeast-1):")
-                #   アクセスキー、シークレットアクセスキー、リージョンを指定し、s3クライアントインスタンスを作成
-                s3_client = boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name=bucket_region)
-            elif select == "n":
-                s3_client = boto3.client("s3")
+            #   アクセスキー、シークレットアクセスキー、リージョンを設定するか？
+            select = input("Set your access key, secret access key, and region?(y/n):")
+            if select in ("y", "n"):
+                break
             else:
                 continue
-            #   指定したバケット内の、指定したプレフィックスを持つファイルのリストを取得する。
-            response = s3_client.list_objects(Bucket=download_bucket_name, Prefix=target_prefix)
-            download_list = [object_data["Key"] for object_data in response["Contents"]]
-            
-            #   ファイルのダウンロード処理
-            for download_key in download_list:
-                #   ディレクトリの場合、何も実行せず次の繰り返し処理に移る。
-                if download_key[-1] == "/":
-                    continue
-                download_file_name = os.path.basename(download_key)
-                #   ファイルをダウンロード
-                s3_client.download_file(Bucket=download_bucket_name, Key=download_key, Filename=download_file_name)
-            break
+        if select == "y":
+            #   アクセスキー、シークレットアクセスキー、リージョンを設定
+            access_key = input("access key:")
+            secret_access_key = input("secret_access_key:")
+            bucket_region = input("s3_bucket_region(Example[Tokyo]: ap-northeast-1):")
+            #   アクセスキー、シークレットアクセスキー、リージョンを指定し、s3クライアントインスタンスを作成
+            s3_client = boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name=bucket_region)
+        elif select == "n":
+             #   s3クライアントインスタンスを作成
+            s3_client = boto3.client("s3")
+        #   指定したバケット内の、指定したプレフィックスを持つファイルのリストを取得する。
+        response = s3_client.list_objects(Bucket=download_bucket_name, Prefix=target_prefix)
+        download_list = [object_data["Key"] for object_data in response["Contents"]]
+        
+        #   ファイルのダウンロード処理
+        for download_key in download_list:
+            #   ディレクトリの場合、何も実行せず次の繰り返し処理に移る。
+            if download_key[-1] == "/":
+                continue
+            download_file_name = os.path.basename(download_key)
+            #   ファイルをダウンロード
+            s3_client.download_file(Bucket=download_bucket_name, Key=download_key, Filename=download_file_name)
     except ClientError as e:
+        #   エラーメッセージを出力
         logging.error(e)
         return False
     return True
@@ -41,45 +45,49 @@ def func_download_files(download_bucket_name: str, target_prefix: str) -> None:
 
 try:
     while True:
-        #   条件分岐：プレフィックスを指定し、配下のファイルを全てダウンロードするか？
+        #   プレフィックスを指定し、配下のファイルを全てダウンロードするか？
         select_1 = input("Specify a prefix and download all files under it?(y/n):")
-        if select_1 == "y":
-            #   バケット名、プレフィックス、ダウンロード先のパスを入力
-            bucket = input("bucket name:")
-            prefix = input("prefix in s3 bucket(Example: log/security):")
-            path = input("Download path:")
-            #   カレントディレクトリを、ダウンロード先ディレクトリに移動
-            os.chdir(path)
-            #   aws s3の指定したバケット内の、指定したプレフィックスを持つファイルを全てダウンロード
-            func_download_files(bucket, prefix)
-        elif select_1 == "n":
-            #   バケット名、ファイル名、ダウンロード先のパスを入力
-            bucket = input("bucket name:")
-            file = input("Key of file in s3 bucket(Example: text/test.txt):")
-            path = input("Download path:")
-            #   カレントディレクトリを、ダウンロード先ディレクトリに移動
-            os.chdir(path)
-            while True:
-                select_2 = input("Select whether to set an access key, secret access key, and region.(y/n):")
-                if select_2 == "y":
-                    #   アクセスキー、シークレットアクセスキー、リージョンを設定
-                    access_key = input("access key:")
-                    secret_access_key = input("secret_access_key:")
-                    bucket_region = input("s3_bucket_region(Example[Tokyo]: ap-northeast-1):")
-                    #   アクセスキー、シークレットアクセスキー、リージョンを指定し、s3クライアントインスタンスを作成
-                    s3_client = boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name=bucket_region)
-                elif select_2 == "n":
-                    #   s3クライアントインスタンスを作成
-                    s3_client = boto3.client("s3")
-                else:
-                    continue
-                #   ファイルをダウンロード
-                file_name = os.path.basename(file)
-                s3_client.download_file(bucket, file, file_name)
-                break
+        if select_1 in ("y", "n"):
+            break
         else:
             continue
-        break
+    if select_1 == "y":
+        #   バケット名、プレフィックス、ダウンロード先のパスを入力
+        bucket = input("bucket name:")
+        prefix = input("prefix in s3 bucket(Example: log/security):")
+        path = input("Download path:")
+        #   カレントディレクトリを、ダウンロード先ディレクトリに移動
+        os.chdir(path)
+        #   aws s3の指定したバケット内の、指定したプレフィックスを持つファイルを全てダウンロード
+        func_download_files(bucket, prefix)
+    elif select_1 == "n":
+        #   バケット名、ファイル名、ダウンロード先のパスを入力
+        bucket = input("bucket name:")
+        file = input("Key of file in s3 bucket(Example: text/test.txt):")
+        path = input("Download path:")
+        #   カレントディレクトリを、ダウンロード先ディレクトリに移動
+        os.chdir(path)
+        while True:
+            #   アクセスキー、シークレットアクセスキー、リージョンを設定するか？
+            select_2 = input("Set your access key, secret access key, and region?(y/n):")
+            if select_2 in ("y", "n"):
+                break
+            else:
+                continue
+        if select_2 == "y":
+            #   アクセスキー、シークレットアクセスキー、リージョンを設定
+            access_key = input("access key:")
+            secret_access_key = input("secret_access_key:")
+            bucket_region = input("s3_bucket_region(Example[Tokyo]: ap-northeast-1):")
+            #   アクセスキー、シークレットアクセスキー、リージョンを指定し、s3クライアントインスタンスを作成
+            s3_client = boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_access_key, region_name=bucket_region)
+        elif select_2 == "n":
+            #   s3クライアントインスタンスを作成
+            s3_client = boto3.client("s3")
+        #   ファイルをダウンロード
+        file_name = os.path.basename(file)
+        s3_client.download_file(bucket, file, file_name)
 except ClientError as e:
+    #   エラーメッセージを出力
     logging.error(e)
 input("続行するには何かキーを押してください...")
